8e49c0eb7ac818ee2a60f4b4dc13aac11fc44821
Rob W <rob@winikates.com>
2016-12-15 09:54:27 -0500

    update to use bing-mobile macro

--- bing.bat
+++ bing.bat
@@ -3,5 +3,5 @@ ECHO start firefox and wait 30 seconds
 start /B "C:\Program Files (x86)\Mozilla Firefox\firefox.exe"
 timeout 5
 ECHO now running the macro...
-"C:\Program Files (x86)\Mozilla Firefox\firefox.exe" imacros://run/?m=bing-total.iim
+"C:\Program Files (x86)\Mozilla Firefox\firefox.exe" imacros://run/?m=bing-mobile.iim
 ECHO FINISHED!
